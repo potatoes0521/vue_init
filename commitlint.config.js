0418f1ae53276428709c9ab3332617b16c3fde6c
刘洋 <liuyang@bjxiaoshi.cn>
2021-03-10 12:54:07 +0800

    新增Git commit

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: liuYang
+ * @description: 请填写描述信息
+ * @path: 引入路径
+ * @Date: 2021-03-10 12:53:39
+ * @LastEditors: liuYang
+ * @LastEditTime: 2021-03-10 12:53:48
+ * @mustParam: 必传参数
+ * @optionalParam: 选传参数
+ * @emitFunction: 函数
+ */
+module.exports = {
+  extends: ['./node_modules/vue-cli-plugin-commitlint/lib/lint']
+}
